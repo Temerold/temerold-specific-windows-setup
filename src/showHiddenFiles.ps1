68458556775dfb824cc64adb9ffe41bea362a505
Temerold <tobbe@temerold.se>
2025-06-14 14:25:40 +0200

    style(showHiddenFiles.ps1): :art: change double quotes to single quotes where interchangeable

--- src/showHiddenFiles.ps1
+++ src/showHiddenFiles.ps1
@@ -1,4 +1,4 @@
 ### Show hidden files
 
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Value 1 -Force
-Write-Output "Enabled showing hidden files"
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'Hidden' -Value 1 -Force
+Write-Output 'Enabled showing hidden files'
