69e47de450e8d6529ed22c2b0e74873d1599b81f
Temerold <tobbe@temerold.se>
2025-06-14 04:57:35 +0200

    feat(disableStartupProcesses.ps1): :sparkles: add src/disableStartupProcesses.ps1

---
+++ src/disableStartupProcesses.ps1
@@ -0,0 +1,51 @@
+### Disable startup processes
+
+$runKeys = @(
+    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run",
+    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Run",
+    "HKLM:\Software\WOW6432Node\Microsoft\Windows\CurrentVersion\Run"
+)
+
+foreach ($key in $runKeys) {
+    try {
+        $processes = Get-ItemProperty -Path $key
+        foreach ($property in $processes.PSObject.Properties) {
+            if ($property.Name -in @("PSPath", "PSParentPath", "PSChildName", "PSDrive", "PSProvider")) { continue }
+            try {
+                Remove-ItemProperty -Path $key -Name $property.Name
+                Write-Output "Disabled startup process: $( $property.Name )"
+            }
+            catch {
+                Write-Error "Could not disable startup process: $( $property.Name )"
+            }
+        }
+    }
+    catch {
+        Write-Error "Could not access registry key: $key"
+    }
+}
+
+$startupDirs = @(
+    "$env:AppData\Microsoft\Windows\Start Menu\Programs\Startup",
+    "$env:ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+)
+
+$removalAttempts = 0
+$maxRemovalAttempts = 5
+$delayMilliseconds = 100
+foreach ($dir in $startupDirs) {
+    if (Test-Path -Path $dir) {
+        Get-ChildItem -Path $dir | ForEach-Object {
+            do {
+                Remove-Item -Path $_.FullName -Force
+                $removalAttempts++
+                Start-Sleep -Milliseconds $delayMilliseconds
+            } while ((Test-Path -Path $_.FullName) -and ($removalAttempts -lt $maxRemovalAttempts))
+            if (Test-Path -Path $_.FullName) {
+                Write-Error "Could not disable startup process: $( $_.Name )"
+                continue
+            }
+            Write-Output "Disabled startup process: $( $_.Name )"
+        }
+    }
+}
