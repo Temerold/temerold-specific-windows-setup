a3768ec7d7319423564b2b9f5851fef3eca4fe69
Temerold <tobbe@temerold.se>
2025-06-14 14:24:38 +0200

    style(installCustomApps.ps1): :art: change double quotes to single quotes where interchangeable

--- src/installCustomApps.ps1
+++ src/installCustomApps.ps1
@@ -4,54 +4,54 @@ $silent = $true  # Set to `$false` to disable silent mode
 
 # Check if WinGet is installed
 if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
-    Write-Error "WinGet is not installed. Please install it first."
+    Write-Error 'WinGet is not installed. Please install it first.'
     exit 1
 }
 
 $apps = @(
-    "7zip.7zip",
-    "Audacity.Audacity",
-    "AudioRangerIT.AudioRanger",
-    @(  "Blizzard.BattleNet",
-        "--location",
-        "C:\Program Files (x86)\Battle.net"),
-    "BoanAnbo.ZoteroCitationPicker",
-    "calibre.calibre",
-    "CrystalDewWorld.CrystalDiskInfo",
-    "CrystalDewWorld.CrystalDiskMark",
-    "Discord.Discord",
-    "dotPDN.PaintDotNet",
-    "Duplicati.Duplicati",
-    "EpicGames.EpicGamesLauncher",
-    "Git.Git",
-    "GNU.Wget2",
-    "Google.Chrome",
-    "Gyan.FFmpeg",
-    "KDE.Kdenlive",
-    "KeePassXCTeam.KeePassXC",
-    "Klocman.BulkCrapUninstaller",
-    "LibreWolf.LibreWolf",
-    "Microsoft.Powershell",
-    "Microsoft.Sysinternals",
-    @(  "Microsoft.VisualStudioCode",
-        "--override",
-        "/mergetasks=!runcode,addcontextmenufiles,addcontextmenufolders /verysilent"),
-    "MiniTool.PartitionWizard.Free",
-    "Modrinth.ModrinthApp",
-    "Mojang.MinecraftLauncher",
-    "Mozilla.Firefox",
-    "OBSProject.OBSStudio",
-    "OpenJS.NodeJS",
-    "Oracle.VirtualBox",
-    "Python.Python.3.13",
-    "qBittorrent.qBittorrent",
-    "RARLab.WinRAR",
-    "Spotify.Spotify",
-    "undergroundwires.privacy.sexy",
-    "Valve.Steam",
-    "VideoLAN.VLC",
-    "WinDirStat.WinDirStat",
-    "yt-dlp.yt-dlp"
+    '7zip.7zip',
+    'Audacity.Audacity',
+    'AudioRangerIT.AudioRanger',
+    @(  'Blizzard.BattleNet',
+        '--location',
+        'C:\Program Files (x86)\Battle.net'),
+    'BoanAnbo.ZoteroCitationPicker',
+    'calibre.calibre',
+    'CrystalDewWorld.CrystalDiskInfo',
+    'CrystalDewWorld.CrystalDiskMark',
+    'Discord.Discord',
+    'dotPDN.PaintDotNet',
+    'Duplicati.Duplicati',
+    'EpicGames.EpicGamesLauncher',
+    'Git.Git',
+    'GNU.Wget2',
+    'Google.Chrome',
+    'Gyan.FFmpeg',
+    'KDE.Kdenlive',
+    'KeePassXCTeam.KeePassXC',
+    'Klocman.BulkCrapUninstaller',
+    'LibreWolf.LibreWolf',
+    'Microsoft.Powershell',
+    'Microsoft.Sysinternals',
+    @(  'Microsoft.VisualStudioCode',
+        '--override',
+        '/mergetasks=!runcode,addcontextmenufiles,addcontextmenufolders /verysilent'),
+    'MiniTool.PartitionWizard.Free',
+    'Modrinth.ModrinthApp',
+    'Mojang.MinecraftLauncher',
+    'Mozilla.Firefox',
+    'OBSProject.OBSStudio',
+    'OpenJS.NodeJS',
+    'Oracle.VirtualBox',
+    'Python.Python.3.13',
+    'qBittorrent.qBittorrent',
+    'RARLab.WinRAR',
+    'Spotify.Spotify',
+    'undergroundwires.privacy.sexy',
+    'Valve.Steam',
+    'VideoLAN.VLC',
+    'WinDirStat.WinDirStat',
+    'yt-dlp.yt-dlp'
 )
 
 foreach ($app in $apps) {
@@ -66,14 +66,14 @@ foreach ($app in $apps) {
     Write-Output "Installing: $appId..."
 
     $winGetArgs = @(
-        "install",
-        "--id", $appId,
-        "--exact",
-        "--accept-source-agreements",
-        "--accept-package-agreements",
-        "--verbose-logs"
+        'install',
+        '--id', $appId,
+        '--exact',
+        '--accept-source-agreements',
+        '--accept-package-agreements',
+        '--verbose-logs'
     ) + $additionalWinGetArgs
-    if ($silent) { $winGetArgs += "--silent" }
+    if ($silent) { $winGetArgs += '--silent' }
 
     winget @winGetArgs
     Write-Output @winGetArgs
