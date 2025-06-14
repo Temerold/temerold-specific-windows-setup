7e0498f573cf8912e9b45b6442eb22af3f4f80de
Temerold <tobbe@temerold.se>
2025-06-14 13:22:35 +0200

    style(addCustomGitIdentity.ps1): :art: change double quotes to single quotes where interchangeable

--- src/addCustomGitIdentity.ps1
+++ src/addCustomGitIdentity.ps1
@@ -1,8 +1,8 @@
 ### Add custom Git identity
 
-git config --global user.name "Temerold"
-Write-Host "✅ Set Git user.name to 'Temerold'" -foregroundcolor Green
+git config --global user.name 'Temerold'
+Write-Host '✅ Set Git user.name to ''Temerold''' -foregroundcolor Green
 
-git config --global user.email "tobbe@temerold.se"
-Write-Host "✅ Set Git user.email to 'tobbe@temerold.se'" -foregroundcolor Green
+git config --global user.email 'tobbe@temerold.se'
+Write-Host '✅ Set Git user.email to '''tobbe@temerold.se'' -foregroundcolor Green
 # TODO: Add Git user.signingkey
