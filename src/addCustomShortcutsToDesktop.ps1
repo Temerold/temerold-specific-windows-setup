fedf2a10953070551e51af5b6b1bdf52903a1f65
Temerold <tobbe@temerold.se>
2025-06-14 13:24:44 +0200

    style(addCustomShortcutsToDesktop.ps1): :art: change double quotes to single quotes where interchangeable

--- src/addCustomShortcutsToDesktop.ps1
+++ src/addCustomShortcutsToDesktop.ps1
@@ -2,22 +2,22 @@
 
 # Check if PowerShell version 7 or higher is being used
 if ($PSVersionTable.PSVersion.Major -lt 7) {
-    Write-Error "This script requires PowerShell version 7 or higher"
+    Write-Error 'This script requires PowerShell version 7 or higher'
     exit 1
 }
 
-$desktopPath = [System.Environment]::GetFolderPath("Desktop")
-$lagringPath = "S:"
+$desktopPath = [System.Environment]::GetFolderPath('Desktop')
+$lagringPath = 'S:'
 $lagringShortcuts = @(
-    @{ Name = "Lagring"; Target = $lagringPath },
-    @{ Name = "Program"; Target = "$lagringPath\Program" },
-    @{ Name = "Dokument"; Target = "$lagringPath\Dokument" },
-    @{ Name = "Böcker"; Target = "$lagringPath\Böcker" },
-    @{ Name = "Musik"; Target = "$lagringPath\Musik" },
-    @{ Name = "Film"; Target = "$lagringPath\Film" },
-    @{ Name = "Memes"; Target = "$lagringPath\Memes" },
-    @{ Name = "Bilder"; Target = "$lagringPath\Bilder" },
-    @{ Name = "Övrigt"; Target = "$lagringPath\Övrigt" }
+    @{ Name = 'Lagring'; Target = $lagringPath },
+    @{ Name = 'Program'; Target = "$lagringPath\Program" },
+    @{ Name = 'Dokument'; Target = "$lagringPath\Dokument" },
+    @{ Name = 'Böcker'; Target = "$lagringPath\Böcker" },
+    @{ Name = 'Musik'; Target = "$lagringPath\Musik" },
+    @{ Name = 'Film'; Target = "$lagringPath\Film" },
+    @{ Name = 'Memes'; Target = "$lagringPath\Memes" },
+    @{ Name = 'Bilder'; Target = "$lagringPath\Bilder" },
+    @{ Name = 'Övrigt'; Target = "$lagringPath\Övrigt" }
 )
 
 foreach ($shortcut in $lagringShortcuts) {
