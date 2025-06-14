a8b366543dcdfdf555ff1fd6e49f6ac48f225026
Temerold <tobbe@temerold.se>
2025-06-14 14:25:23 +0200

    style(showFileExtensions.ps1): :art: change double quotes to single quotes where interchangeable

--- src/showFileExtensions.ps1
+++ src/showFileExtensions.ps1
@@ -1,4 +1,4 @@
 ### Show file extensions
 
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0 -Force
-Write-Output "Enabled showing file extensions"
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideFileExt' -Value 0 -Force
+Write-Output 'Enabled showing file extensions'
