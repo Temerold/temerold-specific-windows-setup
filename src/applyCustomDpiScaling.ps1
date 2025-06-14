25b1b5fa81fc310a189a3ed7f1021cbdf803c930
Temerold <tobbe@temerold.se>
2025-06-14 13:26:36 +0200

    style(applyCustomDpiScaling.ps1): :art: change double quotes to single quotes where interchangeable

--- src/applyCustomDpiScaling.ps1
+++ src/applyCustomDpiScaling.ps1
@@ -2,6 +2,6 @@
 
 $scaling = 1.5
 $dpi = [int](96 * $scaling)
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "Win8DpiScaling" -Type DWord -Value 1
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "LogPixels" -Type DWord -Value $dpi
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name 'Win8DpiScaling' -Type DWord -Value 1
+Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop' -Name 'LogPixels' -Type DWord -Value $dpi
 Write-Output "Applied custom DPI scaling: $dpi"
