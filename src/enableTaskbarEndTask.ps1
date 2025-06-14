1641ce9ad41db57798e88c8e88142467eddd03fe
Temerold <tobbe@temerold.se>
2025-06-14 13:37:56 +0200

    style(enableTaskbarEndTask.ps1): :art: change double quotes to single quotes where interchangeable

--- src/enableTaskbarEndTask.ps1
+++ src/enableTaskbarEndTask.ps1
@@ -1,4 +1,4 @@
 ### Enable taskbar end task
 
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\TaskbarDeveloperSettings" -Name "TaskbarEndTask" -Value 1 -Force
-Write-Output "Enabled taskbar end task"
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\TaskbarDeveloperSettings' -Name 'TaskbarEndTask' -Value 1 -Force
+Write-Output 'Enabled taskbar end task'
