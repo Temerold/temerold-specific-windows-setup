6a4e709dc631fed671fdb7a4ecbef279682256a5
Temerold <tobbe@temerold.se>
2025-06-14 13:37:43 +0200

    style(enableSecondsInSystemClock.ps1): :art: change double quotes to single quotes where interchangeable

--- src/enableSecondsInSystemClock.ps1
+++ src/enableSecondsInSystemClock.ps1
@@ -1,4 +1,4 @@
 ### Enable seconds in system clock
 
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowSecondsInSystemClock" -Value 1 -Force
-Write-Output "Enabled seconds in system clock"
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'ShowSecondsInSystemClock' -Value 1 -Force
+Write-Output 'Enabled seconds in system clock'
