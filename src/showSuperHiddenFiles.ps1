b91725fc8d31793b73bd092201324eb54a5a5fd4
Temerold <tobbe@temerold.se>
2025-06-14 14:26:18 +0200

    style(showSuperHiddenFiles.ps1): :art: change double quotes to single quotes where interchangeable

--- src/showSuperHiddenFiles.ps1
+++ src/showSuperHiddenFiles.ps1
@@ -1,4 +1,4 @@
 ### Show super-hidden files
 
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowSuperHidden" -Value 1 -Force
-Write-Output "Enabled showing super-hidden files"
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'ShowSuperHidden' -Value 1 -Force
+Write-Output 'Enabled showing super-hidden files'
