1435c54302b301298767c325515328575ce304d2
Temerold <tobbe@temerold.se>
2025-06-14 13:37:09 +0200

    style(enableCheckBoxes.ps1): :art: change double quotes to single quotes where interchangeable

--- src/enableCheckBoxes.ps1
+++ src/enableCheckBoxes.ps1
@@ -1,4 +1,4 @@
 ### Enable check boxes
 
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "AutoCheckSelect" -Value 1 -Force
-Write-Output "Enabled check boxes"
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'AutoCheckSelect' -Value 1 -Force
+Write-Output 'Enabled check boxes'
