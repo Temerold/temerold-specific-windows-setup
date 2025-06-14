a11b15d308587c12e9c522ba7a18ff30b9d7d404
Temerold <tobbe@temerold.se>
2025-06-14 04:57:36 +0200

    feat(showFileExtensions.ps1): :sparkles: add src/showFileExtensions.ps1

---
+++ src/showFileExtensions.ps1
@@ -0,0 +1,4 @@
+### Show file extensions
+
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0 -Force
+Write-Output "Enabled showing file extensions"
