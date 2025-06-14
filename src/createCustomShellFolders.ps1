7a069c5e3593df98fc830fb5093a08b43fe622c7
Temerold <tobbe@temerold.se>
2025-06-14 13:34:54 +0200

    style(createCustomShellFolders.ps1): :art: change double quotes to single quotes where interchangeable

--- src/createCustomShellFolders.ps1
+++ src/createCustomShellFolders.ps1
@@ -1,21 +1,21 @@
 ### Create custom shell folders
 
-$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel"
+$registryPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel'
 $registryProperties = Get-ItemProperty -Path $registryPath
 
 # Remove all shell folders
 foreach ($property in $registryProperties.PSObject.Properties) {
-    if ($property.Name -in @("PSChildName", "PSDrive", "PSParentPath", "PSPath", "PSProvider")) { continue }
+    if ($property.Name -in @('PSChildName', 'PSDrive', 'PSParentPath', 'PSPath', 'PSProvider')) { continue }
     Set-ItemProperty -Path $registryPath -Name $property.Name -Value 1 -Force
 }
 
 $desktopIcons = @(
-    "{20D04FE0-3AEA-1069-A2D8-08002B30309D}", # Computer/This PC
-    "{645FF040-5081-101B-9F08-00AA002F954E}"  # Recycle bin
+    '{20D04FE0-3AEA-1069-A2D8-08002B30309D}', # Computer/This PC
+    '{645FF040-5081-101B-9F08-00AA002F954E}'  # Recycle bin
 )
 
 foreach ($propertyName in $desktopIcons) {
     Set-ItemProperty -Path $registryPath -Name $propertyName -Value 0 -Force
 }
 
-Stop-Process -Name "Explorer"
+Stop-Process -Name 'Explorer'
