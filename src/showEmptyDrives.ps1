b649818cc5c3fba8439b9a268f4628188c12c599
Temerold <tobbe@temerold.se>
2025-06-14 14:25:08 +0200

    style(showEmptyDrives.ps1): :art: change double quotes to single quotes where interchangeable

--- src/showEmptyDrives.ps1
+++ src/showEmptyDrives.ps1
@@ -1,4 +1,4 @@
 ### Show empty drives
 
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideDrivesWithNoMedia" -Value 0 -Force
-Write-Output "Enabled showing empty drives"
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'HideDrivesWithNoMedia' -Value 0 -Force
+Write-Output 'Enabled showing empty drives'
