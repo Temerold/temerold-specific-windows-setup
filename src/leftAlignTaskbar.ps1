47a1b6f9d375f1cb398810c39f9cb9852b5c2345
Temerold <tobbe@temerold.se>
2025-06-15 03:36:34 +0200

    feat(leftAlignTaskbar.ps1): :sparkles: add `leftAlignTaskbar.ps1`

---
+++ src/leftAlignTaskbar.ps1
@@ -0,0 +1,4 @@
+### Left align taskbar
+
+Set-ItemProperty -Path HKCU:\software\microsoft\windows\currentversion\explorer\advanced -Name 'TaskbarAl' -Type 'DWord' -Value 0
+Write-Host '✅ Left-aligned taskbar' -Foregroundcolor Green
