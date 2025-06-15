8b4d7fb38280d90164a680f23c70729cbb5059d6
Temerold <tobbe@temerold.se>
2025-06-15 03:09:43 +0200

    fix(addCustomGitIdentity.ps1): :speech_balloon: correct string formatting for `user.email` confirmation message

--- src/addCustomGitIdentity.ps1
+++ src/addCustomGitIdentity.ps1
@@ -4,5 +4,5 @@ git config --global user.name 'Temerold'
 Write-Host '✅ Set Git user.name to ''Temerold''' -Foregroundcolor Green
 
 git config --global user.email 'tobbe@temerold.se'
-Write-Host '✅ Set Git user.email to '''tobbe@temerold.se'' -Foregroundcolor Green
+Write-Host '✅ Set Git user.email to ''tobbe@temerold.se''' -Foregroundcolor Green
 # TODO: Add Git user.signingkey
