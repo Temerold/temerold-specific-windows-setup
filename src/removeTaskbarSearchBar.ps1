a9ebfecb2ecd9964e355309f588dea6719ea47b6
Temerold <tobbe@temerold.se>
2025-06-15 03:45:53 +0200

    feat(removeTaskbarSearchBar.ps1): :sparkles: add `removeTaskbarSearchBar.ps1`

---
+++ src/removeTaskbarSearchBar.ps1
@@ -0,0 +1,4 @@
+### Remove taskbar search bar
+
+Set-ItemProperty -Path HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search -Name 'SearchboxTaskbarMode' -Type 'DWord' -Value 0
+Write-Host '✅ Removed taskbar search bar' -ForegroundColor Green
