1bafc55e51713ef7d55efea880e7340535724d80
Temerold <tobbe@temerold.se>
2025-06-15 17:14:23 +0200

    feat(disableHibernation.ps1): :speech_balloon: add check to confirmation message

--- src/disableHibernation.ps1
+++ src/disableHibernation.ps1
@@ -1,4 +1,8 @@
 ### Disable hibernation
 
-powercfg.exe /hibernate off
-Write-Host '✅ Diabled hibernation' -ForegroundColor 'green'
+powercfg.exe -Hibernate 'off'
+if ( Test-Path 'C:\hiberfil.sys' ) {
+    Write-Error 'Could not disable hibernation'
+    exit 1
+}
+Write-Host '✅ Successfully disabled hibernation' -ForegroundColor 'green'
