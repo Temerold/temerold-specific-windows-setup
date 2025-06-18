85c1340993a9f0e02d83ce8d5c5223a382bc5636
Temerold <tobbe@temerold.se>
2025-06-18 22:15:52 +0200

    fix(addCustomDirectoryToPath.ps1): :speech_balloon: remove redundant debug print

--- src/addCustomDirectoryToPath.ps1
+++ src/addCustomDirectoryToPath.ps1
@@ -3,5 +3,4 @@
 $dir = 'S:\Program\PATH'
 
 [System.Environment]::SetEnvironmentVariable("Path", $env:Path + ";$dir", [System.EnvironmentVariableTarget]::User)
-$env:Path
 Write-Host "Added $dir to path"
