f8a2df5a6429cee49993648263f58187054a3969
Temerold <tobbe@temerold.se>
2025-06-25 13:24:17 +0200

    fix(installMicrosoftOffice.ps1): :bug: add ensuring of PS version > 7 for UTF-8 handling
    The Microsoft Office setup is located in `S:\Övrigt\OfficeSetup.exe`. To consistently handle the Ö correctly, ensure that PowerShell version 7 or greater is being used.

--- src/installMicrosoftOffice.ps1
+++ src/installMicrosoftOffice.ps1
@@ -1,4 +1,10 @@
 ### Install Microsoft Office
 
+# Check if PowerShell version 7 or higher is being used
+if ($PSVersionTable.PSVersion.Major -lt 7) {
+    Write-Error 'This script requires PowerShell version 7 or higher'
+    exit 1
+}
+
 Start-Process 'S:\Övrigt\OfficeSetup.exe' -Wait
 Write-Host "✅ Installed Microsoft Office"
