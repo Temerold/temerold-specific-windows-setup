613a2d7e4941669990561b5ca145d3e52a032f90
Temerold <tobbe@temerold.se>
2025-06-27 19:01:00 +0200

    docs(leftAlignTaskbar.ps1): :pencil2: left align -> left-aligned

--- src/leftAlignTaskbar.ps1
+++ src/leftAlignTaskbar.ps1
@@ -1,4 +1,4 @@
-### Left align taskbar
+### Left-align taskbar
 
 Set-ItemProperty -Path HKCU:\software\microsoft\windows\currentversion\explorer\advanced -Name 'TaskbarAl' -Type 'DWord' -Value 0
 Write-Host '✅ Left-aligned taskbar' -ForegroundColor 'green'
