e19e65f5c3168fa073d4948fc52335f6fb6f9b5e
Temerold <tobbe@temerold.se>
2025-07-01 12:27:06 +0200

    fix(installCustomApps.ps1): :bug: replace `runas` privilege dropping with temporary scheduled task

--- src/installCustomApps.ps1
+++ src/installCustomApps.ps1
@@ -1,4 +1,4 @@
-### Install custom apps with WinGet
+### Install custom apps
 
 param (
     # Don't install apps silently
@@ -12,43 +12,43 @@ if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
 }
 
 $apps = @(
-    #'7zip.7zip',
-    #'AntibodySoftware.WizTree',
-    #'Audacity.Audacity',
-    #'AudioRangerIT.AudioRanger',
-    #@( 'Blizzard.BattleNet', '--location', 'C:\Program Files (x86)\Battle.net' ),
-    #'BoanAnbo.ZoteroCitationPicker',
-    #'calibre.calibre',
-    #'CrystalDewWorld.CrystalDiskInfo',
-    #'CrystalDewWorld.CrystalDiskMark',
-    #'Discord.Discord',
-    #'dotPDN.PaintDotNet',
-    #'Duplicati.Duplicati',
-    #'EpicGames.EpicGamesLauncher',
-    #'Git.Git',
-    #'GNU.Wget2',
-    #'Google.Chrome',
-    #'Gyan.FFmpeg',
-    #'KDE.Kdenlive',
-    #'KeePassXCTeam.KeePassXC',
-    #'Klocman.BulkCrapUninstaller',
-    #'LibreWolf.LibreWolf',
-    #'Microsoft.PowerShell',
-    #'Microsoft.Sysinternals',
-    #@(  'Microsoft.VisualStudioCode',
-    #    '--override',
-    #    '/mergetasks=!runcode,addcontextmenufiles,addcontextmenufolders /verysilent'),
-    #'MiniTool.PartitionWizard.Free',
-    #'Modrinth.ModrinthApp',
-    #'Mojang.MinecraftLauncher',
-    #'Mozilla.Firefox',
-    #'MusicBrainz.Picard',
-    #'OBSProject.OBSStudio',
-    #'OpenJS.NodeJS',
-    #'Oracle.VirtualBox',
-    #'Python.Python.3.13',
-    #'qBittorrent.qBittorrent',
-    #'RARLab.WinRAR',
+    '7zip.7zip',
+    'AntibodySoftware.WizTree',
+    'Audacity.Audacity',
+    'AudioRangerIT.AudioRanger',
+    @( 'Blizzard.BattleNet', '--location', 'C:\Program Files (x86)\Battle.net' ),
+    'BoanAnbo.ZoteroCitationPicker',
+    'calibre.calibre',
+    'CrystalDewWorld.CrystalDiskInfo',
+    'CrystalDewWorld.CrystalDiskMark',
+    'Discord.Discord',
+    'dotPDN.PaintDotNet',
+    'Duplicati.Duplicati',
+    'EpicGames.EpicGamesLauncher',
+    'Git.Git',
+    'GNU.Wget2',
+    'Google.Chrome',
+    'Gyan.FFmpeg',
+    'KDE.Kdenlive',
+    'KeePassXCTeam.KeePassXC',
+    'Klocman.BulkCrapUninstaller',
+    'LibreWolf.LibreWolf',
+    'Microsoft.PowerShell',
+    'Microsoft.Sysinternals',
+    @(  'Microsoft.VisualStudioCode',
+        '--override',
+        '/mergetasks=!runcode,addcontextmenufiles,addcontextmenufolders /verysilent'),
+    'MiniTool.PartitionWizard.Free',
+    'Modrinth.ModrinthApp',
+    'Mojang.MinecraftLauncher',
+    'Mozilla.Firefox',
+    'MusicBrainz.Picard',
+    'OBSProject.OBSStudio',
+    'OpenJS.NodeJS',
+    'Oracle.VirtualBox',
+    'Python.Python.3.13',
+    'qBittorrent.qBittorrent',
+    'RARLab.WinRAR',
     @('Spotify.Spotify', '--nonadmin'),
     'undergroundwires.privacy.sexy',
     'Valve.Steam',
@@ -77,32 +77,37 @@ foreach ($app in $apps) {
     ) + $additionalWinGetArgs
     if (-not $loud) { $winGetArgs += '--silent' }
 
-    if ($winGetArgs -contains '--nonadmin') {
+    $elevated = [System.Security.Principal.WindowsIdentity]::GetCurrent().Groups -contains "S-1-5-32-544"
+    if ($winGetArgs -contains '--nonadmin' -and $elevated) {
         Write-Host "Running in non-admin mode for: $appId"
         $winGetArgs = $winGetArgs | Where-Object { $_ -ne '--nonadmin' }
         $winGetCommand = "winget $( $winGetArgs -join ' ' )"
 
-        # Create a new PowerShell process with `runas` instead of `ShellExecute` to run in non-admin mode.
-        # When ran with PowerShell 7.5 (not the more commonly installed 5.1), `ShellExecute` inherits elevation
-        # from the parent process, even when the child is 5.1, thus the use of `runas`, which works in
-        # both versions. https://www.reddit.com/r/sysadmin/comments/16kw85h/comment/k0ymmdo/
+        # Create, immediately run, and finally remove a scheduled task installing the app.
+        # This method seems to work consistently, independent of elevation when running this script.
         #
-        # FURTHERMORE, due to a known bug not fixed until PowerShell 7.3.0 when passing embedded quotes in
-        # arguments passed to external programs, double quotes (") can't be escaped using double quotes (" -> "").
-        # Instead, we have to use backslashes (\) AND double quotes. However, this hotfix doesn't work in versions
-        # >= 7.3.0, and so we have to use `$PSNativeCommandArgumentPassing = 'Legacy'` to make sure that all
-        # versions handles it as the old, incorrect ones would.
-        # https://stackoverflow.com/questions/24745868/powershell-passing-json-string-to-curl/66837948#66837948
-        # https://stackoverflow.com/questions/74440303/powershell-7-3-0-breaking-command-invocation/74440425#74440425
-        # https://stackoverflow.com/questions/59036580/pwsh-command-is-removing-quotation-marks/59036879#59036879
-        # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_preference_variables?view=powershell-7.5#psnativecommandargumentpassing
-        $PSNativeCommandArgumentPassing = 'Legacy'
-        runas /trustlevel:0x20000 "powershell -Command \""$winGetCommand\"""
+        # Prior attempts to make this work included creating a new PowerShell process with `runas`, which had
+        # inconsistent success across fresh installs of Windows 11, and therefore has been abandoned. The main issue
+        # is the new process inheriting elevation from the parent process. Read more in the Git history.
+        $taskName = "Install $appId"
+        $taskDescription = "Installs $appId with WinGet"
+        $action = New-ScheduledTaskAction -Execute "powershell" -Argument "-Command `"$winGetCommand`""
+        $trigger = New-ScheduledTaskTrigger -AtLogOn
+        $userId = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name
+        $principal = New-ScheduledTaskPrincipal -UserId $userId -LogonType Interactive
 
-
-        continue
+        if (Get-ScheduledTask | Where-Object { $_.TaskName -eq $taskName }) {
+            Unregister-ScheduledTask -TaskName $taskName -Confirm 0
+        }
+        Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -TaskName $taskName -Description $taskDescription | Out-Null
+        Start-ScheduledTask -TaskName $taskName
+        Unregister-ScheduledTask -TaskName $taskName -Confirm:$false
+        $LASTEXITCODE = 0
     }
-    winget @winGetArgs
+    else {
+        $winGetArgs = $winGetArgs | Where-Object { $_ -ne '--nonadmin' }
+        winget @winGetArgs
+    }
 
     # https://github.com/microsoft/winget-cli/blob/master/doc/windows/package-manager/winget/returnCodes.md
     # Exit code -1978335189 means that the app is already installed and has no available update.
